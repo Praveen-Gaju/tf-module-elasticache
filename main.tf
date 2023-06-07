3d03eb75b7155e64703fb57d8affc73204981abb
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-07 18:19:22 +0530

    updating tf-module-elasticache directory

--- main.tf
+++ main.tf
@@ -23,6 +23,34 @@ resource "aws_elasticache_subnet_group" "main" {
   )
 }
 
+#security group
+resource "aws_security_group" "main" {
+  name        = "${var.env}-elasticache"
+  description = "${var.env}-elasticache"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "ELASTICACHE"
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags       = merge(
+    var.tags,
+    { Name = "${var.env}-elasticache" }
+  )
+}
+
+
 #elasticache endpoint parameter
 resource "aws_ssm_parameter" "ealsticache_endpoint" {
   name  = "${var.env}.elasticache.endpoint"
--- vars.tf
+++ vars.tf
@@ -6,3 +6,5 @@ variable "engine" {}
 variable "engine_version" {}
 variable "num_cache_nodes" {}
 variable "node_type" {}
+variable "allow_subnets" {}
+variable "vpc_id" {}
